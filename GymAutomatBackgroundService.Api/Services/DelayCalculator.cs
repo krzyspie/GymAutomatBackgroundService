063bc4e4d9d36914051741ae84efccb4af569ae5
krzpie <krzys_7@op.pl>
2025-05-29 19:10:09 +0200

    Delay calculator implementation

--- GymAutomatBackgroundService.Api/Services/DelayCalculator.cs
+++ GymAutomatBackgroundService.Api/Services/DelayCalculator.cs
@@ -4,6 +4,23 @@ public class DelayCalculator : IDelayCalculator
 {
     public int CalculateDelay()
     {
-        return 0;
+        DateTime now = DateTime.Now;
+        DateTime yogaRegistrationDay = now.DayOfWeek switch
+        {
+            DayOfWeek.Friday => now.AddDays(5),
+            DayOfWeek.Saturday => now.AddDays(4),
+            DayOfWeek.Sunday => now.AddDays(3),
+            DayOfWeek.Monday => now.AddDays(2),
+            DayOfWeek.Tuesday => now.AddDays(2),
+            DayOfWeek.Wednesday => now.AddDays(2),
+            DayOfWeek.Thursday => now.AddDays(1),
+            _ => now
+        };
+
+        DateTime referenceDate = new DateTime(yogaRegistrationDay.Year, yogaRegistrationDay.Month, yogaRegistrationDay.Day, 12, 15, 0);
+
+        var delay = referenceDate - now;
+        
+        return delay.Microseconds;
     }
 }
