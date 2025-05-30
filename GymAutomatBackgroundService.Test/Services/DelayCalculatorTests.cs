de3fe7bd9bb1204c112a8805c951cd8729b75606
krzpie <krzys_7@op.pl>
2025-05-30 21:45:51 +0200

    Add unit test project

---
+++ GymAutomatBackgroundService.Test/Services/DelayCalculatorTests.cs
@@ -0,0 +1,15 @@
+namespace GymAutomatBackgroundService.Test.Services;
+
+public class DelayCalculatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
