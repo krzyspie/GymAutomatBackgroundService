552b6563872cd8688de885ecf793014c4b994576
krzpie <krzys_7@op.pl>
2025-06-11 13:43:05 +0200

    unit test

--- GymAutomatBackgroundService.Test/Services/DelayCalculatorTests.cs
+++ GymAutomatBackgroundService.Test/Services/DelayCalculatorTests.cs
@@ -1,15 +1,32 @@
+using GymAutomatBackgroundService.Api.Services;
+using GymAutomatBackgroundService.Api.Utilities;
+using Moq;
+
 namespace GymAutomatBackgroundService.Test.Services;
 
 public class DelayCalculatorTests
 {
+    private Mock<IDateTimeProvider> _dateTimeProviderMock;
+    private DelayCalculator _delayCalculator;
+    
     [SetUp]
     public void Setup()
     {
+        _dateTimeProviderMock = new Mock<IDateTimeProvider>();
+        _delayCalculator = new DelayCalculator(_dateTimeProviderMock.Object);
     }
 
-    [Test]
-    public void Test1()
+    [TestCase(11)]
+    [TestCase(13)]
+    public void ShouldReturnOneSecond_WhenWendsdayAndFriday(int day)
     {
-        Assert.Pass();
+        //Arrange
+        _dateTimeProviderMock.Setup(x => x.Now).Returns(new DateTime(2025, 06, day, 12, 30, 45));
+        
+        //Act
+        var result = _delayCalculator.CalculateDelay();
+        
+        //Assert
+        Assert.That(result, Is.EqualTo(1000));
     }
 }
