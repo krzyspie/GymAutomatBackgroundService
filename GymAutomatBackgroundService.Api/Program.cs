1c0024750aa1ea235569f441785c069977f7a535
krzpie <krzys_7@op.pl>
2025-06-14 16:48:34 +0200

    cleanup

--- GymAutomatBackgroundService.Api/Program.cs
+++ GymAutomatBackgroundService.Api/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IDelayCalculator, DelayCalculator>();
 builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
 builder.Services.AddHostedService<GymAutomat>();
+
 var app = builder.Build();
 
 app.Run();
