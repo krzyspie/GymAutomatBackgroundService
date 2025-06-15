09bf142cd42d0d3905295f89a3957e83b650eaba
krzpie <krzys_7@op.pl>
2025-06-15 22:57:29 +0200

    Tests

--- GymAutomatBackgroundService.Test/Services/DelayCalculatorTests.cs
+++ GymAutomatBackgroundService.Test/Services/DelayCalculatorTests.cs
@@ -27,6 +27,24 @@ public class DelayCalculatorTests
         var result = _delayCalculator.CalculateDelay();
         
         //Assert
-        Assert.That(result, Is.EqualTo(1000));
+        Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(1)));
+    }
+    
+    [TestCase(2, 2)]
+    [TestCase(3, 1)]
+    [TestCase(5, 1)]
+    [TestCase(7, 4)]
+    [TestCase(8, 3)]
+    public void ShouldReturnDays_ForGivenDay(int day, int expectedDate)
+    {
+        //Arrange
+        var date = new DateTime(2025, 06, day, 12, 15, 00, DateTimeKind.Local);
+        _dateTimeProviderMock.Setup(x => x.Now).Returns(date);
+        
+        //Act
+        var result = _delayCalculator.CalculateDelay();
+        
+        //Assert
+        Assert.That(result, Is.EqualTo(TimeSpan.FromDays(expectedDate)));
     }
 }
