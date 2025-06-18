cf5f47934719a252401d17bdd778c0375e12b297
krzpie <krzys_7@op.pl>
2025-06-18 21:22:50 +0200

    Register services

--- GymAutomatBackgroundService.Api/Program.cs
+++ GymAutomatBackgroundService.Api/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IDelayCalculator, DelayCalculator>();
 builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+builder.Services.AddSingleton<IGymAccessService, GymAccessService>();
+builder.Services.AddSingleton<IGymWorkoutService, GymWorkoutService>();
 
 builder.Services.AddHostedService<GymAutomat>();
 
