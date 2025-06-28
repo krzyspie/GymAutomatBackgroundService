08382170e7e5477c2061f2575569997b8b4d7e53
krzpie <krzys_7@op.pl>
2025-06-28 14:35:33 +0200

    Add client dependency to service class

--- GymAutomatBackgroundService.Api/Services/GymWorkoutService.cs
+++ GymAutomatBackgroundService.Api/Services/GymWorkoutService.cs
@@ -1,7 +1,16 @@
+using GymAutomatBackgroundService.Api.HttpClients;
+
 namespace GymAutomatBackgroundService.Api.Services;
 
 public class GymWorkoutService : IGymWorkoutService
 {
+    private readonly IGymClient _gymClient;
+    private const string JogaWorkoutId = "33146527";
+
+    public GymWorkoutService(IGymClient gymClient)
+    {
+        _gymClient = gymClient;
+    }
     public void RegisterToJogaClass()
     {
         throw new NotImplementedException();
