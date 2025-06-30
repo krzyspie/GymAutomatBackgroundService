4a3094cbe6031e268d7d3f2294a140fc9e83ad5f
krzpie <krzys_7@op.pl>
2025-06-30 22:12:40 +0200

    Rename

--- GymAutomatBackgroundService.Api/Services/IRequestDataFactory.cs
+++ GymAutomatBackgroundService.Api/Services/IRequestDataFactory.cs
@@ -2,7 +2,7 @@ namespace GymAutomatBackgroundService.Api.Services;
 
 public interface IRequestDataFactory
 {
-    FormUrlEncodedContent GetClassesRequest(string dateFrom, string dateTo);
+    FormUrlEncodedContent GetJogaWorkoutsRequest(string dateFrom, string dateTo);
     FormUrlEncodedContent RegisterWorkoutRequest(string classId);
     FormUrlEncodedContent LoginToGymRequest(string login, string password);
 }
