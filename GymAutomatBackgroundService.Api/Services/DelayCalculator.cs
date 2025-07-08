6824580992a89a0695ccca1033e7258e283436f9
krzpie <krzys_7@op.pl>
2025-07-08 19:36:16 +0200

    rewrite delay calculator

--- GymAutomatBackgroundService.Api/Services/DelayCalculator.cs
+++ GymAutomatBackgroundService.Api/Services/DelayCalculator.cs
@@ -10,30 +10,15 @@ public class DelayCalculator : IDelayCalculator
     {
         _dateTimeProvider = dateTimeProvider;
     }
-    public TimeSpan CalculateDelay()
+    public TimeSpan CalculateDelay(DateTime workoutDate)
     {
+        DateTime registrationDate = workoutDate.AddDays(-3);
+        DateTime registrationDateTime = new DateTime(registrationDate.Year, registrationDate.Month, registrationDate.Day, 12, 20, 0, DateTimeKind.Local);
         DateTime now = _dateTimeProvider.Now;
 
-        if ((now.DayOfWeek == DayOfWeek.Wednesday || now.DayOfWeek == DayOfWeek.Friday) && now.Hour == 12)
-        {
-            return TimeSpan.FromSeconds(1);
-        }
-        
-        DateTime yogaRegistrationDay = now.DayOfWeek switch
-        {
-            DayOfWeek.Friday => now.AddDays(5),
-            DayOfWeek.Saturday => now.AddDays(4),
-            DayOfWeek.Sunday => now.AddDays(3),
-            DayOfWeek.Monday => now.AddDays(2),
-            DayOfWeek.Tuesday => now.AddDays(1),
-            DayOfWeek.Wednesday => now.AddDays(2),
-            DayOfWeek.Thursday => now.AddDays(1),
-            _ => now
-        };
-
-        DateTime referenceDate = new DateTime(yogaRegistrationDay.Year, yogaRegistrationDay.Month, yogaRegistrationDay.Day, 12, 15, 0, DateTimeKind.Local);
-
-        var delay = referenceDate - now;
+        var delay = registrationDateTime > now
+            ? registrationDateTime - now
+            : TimeSpan.FromDays(1);
         
         return delay;
     }
