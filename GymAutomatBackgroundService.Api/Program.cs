f8fd7e217beb1f0e592229636554adb4360f1cb7
krzpie <krzys_7@op.pl>
2025-07-10 19:12:22 +0200

    service registration

--- GymAutomatBackgroundService.Api/Program.cs
+++ GymAutomatBackgroundService.Api/Program.cs
@@ -5,14 +5,17 @@ using GymAutomatBackgroundService.Api.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient<GymClient>( client =>
+builder.Services.AddHttpClient<IGymClient, GymClient>( client =>
 {
     client.BaseAddress = new Uri("https://justgym.pl/");
 });
+
 builder.Services.AddSingleton<IDelayCalculator, DelayCalculator>();
 builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 builder.Services.AddSingleton<IGymAccessService, GymAccessService>();
 builder.Services.AddSingleton<IGymWorkoutService, GymWorkoutService>();
+builder.Services.AddSingleton<IRequestDataFactory, RequestDataFactory>();
+builder.Services.AddSingleton<ISecretsManagerService, SecretsManagerService>();
 
 builder.Services.AddHostedService<GymAutomat>();
 
