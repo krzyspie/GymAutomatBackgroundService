60dd90e0b8968f436610674645fcd19155694821
krzpie <krzys_7@op.pl>
2025-07-11 21:22:15 +0200

    get login and password from gcp secret manager

--- GymAutomatBackgroundService.Api/Services/GymAccessService.cs
+++ GymAutomatBackgroundService.Api/Services/GymAccessService.cs
@@ -6,16 +6,21 @@ public class GymAccessService : IGymAccessService
 {
     private readonly IGymClient _gymClient;
     private readonly IRequestDataFactory _requestDataFactory;
+    private readonly ISecretsManagerService _secretsManagerService;
 
-    public GymAccessService(IGymClient gymClient, IRequestDataFactory requestDataFactory)
+    public GymAccessService(IGymClient gymClient, IRequestDataFactory requestDataFactory, ISecretsManagerService secretsManagerService)
     {
         _gymClient = gymClient;
         _requestDataFactory = requestDataFactory;
+        _secretsManagerService = secretsManagerService;
     }
     
     public async Task Login()
     {
-        FormUrlEncodedContent requestData = _requestDataFactory.LoginToGymRequest("aaa", "bbb");
+        string login = _secretsManagerService.GetLogin();
+        string password = _secretsManagerService.GetPassword();
+        
+        FormUrlEncodedContent requestData = _requestDataFactory.LoginToGymRequest(login, password);
         await _gymClient.LoginToGym(requestData);
     }
 
