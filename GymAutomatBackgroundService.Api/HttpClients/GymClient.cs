6bb1e482759d200dcc1e3f46bd2430a8b70072dc
krzpie <krzys_7@op.pl>
2025-07-12 15:28:10 +0200

    read gym workouts deserialization

--- GymAutomatBackgroundService.Api/HttpClients/GymClient.cs
+++ GymAutomatBackgroundService.Api/HttpClients/GymClient.cs
@@ -19,13 +19,13 @@ public class GymClient : IGymClient
         HttpResponseMessage response = await _client.PostAsync("/wp-admin/admin-ajax.php", data);
         var responseString = await response.Content.ReadAsStringAsync();
         var result = JsonSerializer.Deserialize<GymResponse>(responseString, _jsonSerializerOptions);
-
+        
         return result;
     }
 
     public async Task Logout()
     {
-        HttpResponseMessage response = await _client.GetAsync("/admin-panel/?action=logout&redirect_to=https%3A%2F%2Fjustgym.pl%2Fklient%3Flogout%3Dsuccess&_wpnonce=c1e868a7b7");
+        HttpResponseMessage response = await _client.PostAsync("/admin-panel/?action=logout&redirect_to=https%3A%2F%2Fjustgym.pl%2Fklient%3Flogout%3Dsuccess&_wpnonce=c1e868a7b7", null);
         response.EnsureSuccessStatusCode();
     }
 
@@ -41,8 +41,10 @@ public class GymClient : IGymClient
     public async Task<GymWorkoutsResponse> GetWorkouts(FormUrlEncodedContent data)
     {
         HttpResponseMessage response = await _client.PostAsync("/wp-admin/admin-ajax.php", data);
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonSerializer.Deserialize<GymWorkoutsResponse>(responseString, _jsonSerializerOptions);
+        var responseString = await response.Content.ReadAsStringAsync(); 
+
+        var innerJson = JsonSerializer.Deserialize<string>(responseString);
+        var result = JsonSerializer.Deserialize<GymWorkoutsResponse>(innerJson, _jsonSerializerOptions);
 
         return result ?? new GymWorkoutsResponse();
     }
