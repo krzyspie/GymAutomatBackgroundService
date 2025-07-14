6736c3bf4fa2f4768564c46d23d395af920787c7
krzpie <krzys_7@op.pl>
2025-07-14 17:02:03 +0200

    extend workouts result about participants number

--- GymAutomatBackgroundService.Api/Models/WorkoutResult.cs
+++ GymAutomatBackgroundService.Api/Models/WorkoutResult.cs
@@ -5,4 +5,8 @@ public class WorkoutResult
     public int ClassId { get; set; }
     public required string Name { get; set; }
     public required string StartDate { get; set; }
+    
+    public required int ParticipantsLimit { get; set; }
+    
+    public required int ParticipantsNumber { get; set; }
 }
