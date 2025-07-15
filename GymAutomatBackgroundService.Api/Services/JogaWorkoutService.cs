2f59c06750a4200e59778e89f2d42a99a9c4fe82
krzpie <krzys_7@op.pl>
2025-07-15 19:19:48 +0200

    GetJogaWorkoutToRegister method

---
+++ GymAutomatBackgroundService.Api/Services/JogaWorkoutService.cs
@@ -0,0 +1,11 @@
+using GymAutomatBackgroundService.Api.Models;
+
+namespace GymAutomatBackgroundService.Api.Services;
+
+public class JogaWorkoutService
+{
+    public JogaWorkoutModel GetJogaWorkoutToRegister(List<JogaWorkoutModel> jogaWorkouts)
+    {
+        return null;
+    }
+}
