97fdb16854ca67081afb568862b3179d74bbc500
krzpie <krzys_7@op.pl>
2025-07-16 14:09:15 +0200

    GetJogaWorkoutToRegister implementation

--- GymAutomatBackgroundService.Api/Services/JogaWorkoutService.cs
+++ GymAutomatBackgroundService.Api/Services/JogaWorkoutService.cs
@@ -1,11 +1,34 @@
 using GymAutomatBackgroundService.Api.Models;
+using GymAutomatBackgroundService.Api.Utilities;
 
 namespace GymAutomatBackgroundService.Api.Services;
 
 public class JogaWorkoutService
 {
+    private readonly DateTimeProvider _dateTimeProvider;
+
+    public JogaWorkoutService(DateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
     public JogaWorkoutModel GetJogaWorkoutToRegister(List<JogaWorkoutModel> jogaWorkouts)
     {
+        var workouts = jogaWorkouts
+            .Where(w => w.StartDate.DayOfWeek != DayOfWeek.Sunday)
+            .OrderBy(j => j.StartDate);
+
+        foreach (var jogaWorkout in workouts)
+        {
+            if (_dateTimeProvider.Now <= jogaWorkout.StartDate.AddDays(-4))
+            {
+                return jogaWorkout;
+            }
+            if (jogaWorkout.ParticipantsNumber < jogaWorkout.ParticipantsLimit)
+            {
+                return jogaWorkout;
+            }
+        }
+
         return null;
     }
 }
